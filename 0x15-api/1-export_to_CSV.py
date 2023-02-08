8837f4410fb0302be85803603972278e7d9ff94f
Yofty <106748943+Yofty@users.noreply.github.com>
2023-02-08 11:29:28 +0000

    correction 3

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,18 +7,19 @@ extended to export data in the CSV format
 import re
 import requests
 import sys
+import json
 
 
 API = "https://jsonplaceholder.typicode.com"
 """REST API url"""
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
             id = int(sys.argv[1])
             user_res = requests.get('{}/users/{}'.format(API, id)).json()
-            todos_res = requests.get('{}/todos'.format(API))json()
+            todos_res = requests.get('{}/todos'.format(API)).json()
             user_name = user_res.get('username')
             todos = list(filter(lambda x: x.get('userId') ==id, todos_res))
             with open('{}.csv'.format(id), 'w') as file:
