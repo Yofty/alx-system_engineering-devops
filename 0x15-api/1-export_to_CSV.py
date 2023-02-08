2f0207562ed7664c03c3ba4a5ef8b0ab9a007028
Yofty <106748943+Yofty@users.noreply.github.com>
2023-02-08 11:33:09 +0000

    multiple correction

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,10 +4,10 @@ using https://jsonplaceholder.typicode.com
 returns information about employee TODO list progress
 extended to export data in the CSV format
 """
+import json
 import re
 import requests
 import sys
-import json
 
 
 API = "https://jsonplaceholder.typicode.com"
@@ -21,11 +21,11 @@ if __name__ == '__main__':
             user_res = requests.get('{}/users/{}'.format(API, id)).json()
             todos_res = requests.get('{}/todos'.format(API)).json()
             user_name = user_res.get('username')
-            todos = list(filter(lambda x: x.get('userId') ==id, todos_res))
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
             with open('{}.csv'.format(id), 'w') as file:
                 for todo in todos:
                     file.write(
-                            '"{}", "{}", "{}", "{}"\n'.format(
+                            '"{}","{}","{}","{}"\n'.format(
                                 id,
                                 user_name,
                                 todo.get('completed'),
