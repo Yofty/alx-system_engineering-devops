5b07a27baed15bf40103c48d78f6634703621afc
Yofty <106748943+Yofty@users.noreply.github.com>
2023-02-28 18:04:51 -0800

    correctin

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
         return 0
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers = user_agent)
+    response = get(url, headers=user_agent)
     results = response.json()
     try:
         return results.get('data').get('subscribers')
